be8bd22f1d38038fa127dfb7627357c2ea70caa9
Deizan <deyzen1107@gmail.com>
2026-03-31 09:49:17 +0700

    selection & insertion

--- Praktikum05/SortingMain16.java
+++ Praktikum05/SortingMain16.java
@@ -3,15 +3,35 @@ public class SortingMain16 {
     public static void main(String[] args) {
 
         int a[] = {20, 10, 2, 7, 12};
-
         Sorting16 dataurut1 = new Sorting16(a, a.length);
 
-        System.out.println("Data awal 1");
+        System.out.println("Data awal 1:");
         dataurut1.tampil();
 
         dataurut1.bubbleSort();
-
-        System.out.println("Data setelah diurutkan dengan BUBBLE SORT (ASC):");
+        System.out.println("Data setelah Bubble Sort:");
         dataurut1.tampil();
+
+
+        int b[] = {30, 20, 2, 8, 14};
+        Sorting16 dataurut2 = new Sorting16(b, b.length);
+
+        System.out.println("\nData awal 2:");
+        dataurut2.tampil();
+
+        dataurut2.selectionSort();
+        System.out.println("Data setelah Selection Sort:");
+        dataurut2.tampil();
+
+
+        int c[] = {40, 10, 4, 9, 3};
+        Sorting16 dataurut3 = new Sorting16(c, c.length);
+
+        System.out.println("\nData awal 3:");
+        dataurut3.tampil();
+
+        dataurut3.insertionSort();
+        System.out.println("Data setelah Insertion Sort:");
+        dataurut3.tampil();
     }
 }
